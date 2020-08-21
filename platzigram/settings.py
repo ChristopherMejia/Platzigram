7a5ca2b4019b54293619c35ace60ea53a0c9ceb4
Christopher Mejia <chis.mejia.garcia@gmail.com>
2020-08-21 16:02:03 -0500

    Configuramos la conexion al BDD

--- platzigram/settings.py
+++ platzigram/settings.py
@@ -79,8 +79,13 @@ WSGI_APPLICATION = 'platzigram.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'platzigram',
+        'USER': 'root',
+        'PASSWORD': 'cemg23',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
+        #'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
