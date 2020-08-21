5dc87a5c40c544aa57df6afb748f6c4894b7b70d
Christopher Mejia <chis.mejia.garcia@gmail.com>
2020-08-20 19:18:47 -0500

    Agregamos nombre, user, timestamp y picture a la vista

--- posts/views.py
+++ posts/views.py
@@ -5,19 +5,40 @@ from django.http import HttpResponse
 #Utilities
 from datetime import datetime
 
-posts[
+posts = [
+
     {
         'name' : 'Sayulita',
         'user' : 'Yvonne Alonso',
         'timestamp' : datetime.now().strftime('%b %dth, %Y - %H:%M hrs'),
-        'picture' : 'https://picsum.photos/200/200/?image=1036'
-    }
+        'picture' : 'https://picsum.photos/200/200/?image=1036',
+    },
+    
+    {
+        'name' : 'Via Lactea',
+        'user' : 'Cesar Caton',
+        'timestamp' : datetime.now().strftime('%b %dth, %Y - %H:%M hrs'),
+        'picture' : 'https://picsum.photos/200/200/?image=903',
+    },
+    
+    {
+        'name' : 'Nuevo Auditorio',
+        'user' : 'Thespianartist',
+        'timestamp' : datetime.now().strftime('%b %dth, %Y - %H:%M hrs'),
+        'picture' : 'https://picsum.photos/200/200/?image=1076',
+    },
 ]
 
 def list_posts(request):
-    posts = [1,2,3]
-
-    return HttpResponse(str(posts))
+    
+    content = []
+    for post in posts:
+        content.append("""
+           <p><strong>{name}</strong></p>
+           <p><small>{user} - <i>{timestamp}</i></small></p> 
+           <figure><img src="{picture}"/></figure> 
+        """.format(**post) )
+    return HttpResponse('<br>'.join(content) )
 
 
 
