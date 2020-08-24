ec637094b6ec787f8220fa3e387468b5a5112e96
Christopher Mejia <chis.mejia.garcia@gmail.com>
2020-08-24 18:11:35 -0500

    Modificamos la tabla User

--- posts/models.py
+++ posts/models.py
@@ -11,6 +11,8 @@ class User(models.Model):
     firs_name = models.CharField(max_length = 50)
     last_name = models.CharField(max_length = 50)
 
+    is_admin = models.BooleanField(default=False)
+
     bio = models.TextField(blank = True)
 
     birthdate = models.DateField(blank = True, null=True)
