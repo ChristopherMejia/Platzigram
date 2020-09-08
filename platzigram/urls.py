f66820687ba12b4c40cc9e05825e71cfda60f311
Christopher Mejia <chis.mejia.garcia@gmail.com>
2020-09-08 17:59:11 -0500

    url de logout y login

--- platzigram/urls.py
+++ platzigram/urls.py
@@ -7,6 +7,7 @@ from django.contrib import admin
 
 from platzigram import views as local_views
 from posts import views as posts_views
+from users import views as users_views
 
 
 
@@ -14,10 +15,14 @@ urlpatterns = [
 
     path('admin/', admin.site.urls),
 
-    path('hello/', local_views.hello), 
-    path('sorted/', local_views.sorted),
-    path('hi/<str:name>/<int:age>/', local_views.hi),
+    path('hello/', local_views.hello, name='hello_wolrd'), 
+    path('sorted/', local_views.sorted, name='sort'),
+    path('hi/<str:name>/<int:age>/', local_views.hi, name="hi" ),
 
-    path('posts/',posts_views.list_posts),
+    path('posts/',posts_views.list_posts , name='feed'),
+
+    path('users/login/', users_views.login_view, name ='login'),
+    path('users/logout/', users_views.logout_view, name ='logout'),
+
     
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
