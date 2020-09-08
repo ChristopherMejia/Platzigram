b0bc888926f63e5a5953f8796b8d94586cd4010a
Christopher Mejia <chis.mejia.garcia@gmail.com>
2020-09-08 18:04:31 -0500

    agregamos la funcion login_view y logout_view

--- users/views.py
+++ users/views.py
@@ -1,3 +1,27 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
+
+def login_view(request):
+    """Login view"""
+    #import pdb; pdb.set_trace()
+
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user:
+            login(request, user)
+            return redirect('feed') 
+        else:
+            return render(request, 'users/login.html', {'error': 'Invalid username and password'})
+
+
+    return render(request, 'users/login.html')
+
+def logout_view(request):
+
+    logout(request)
+    return redirect('login')
